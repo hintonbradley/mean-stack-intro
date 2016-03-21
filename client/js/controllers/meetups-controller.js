0804e9974d58b0f62b6aff673fe0c2807fcb4bd5
James Bradley <hintonbradley@gmail.com>
2016-03-21 15:08:52 -0700

    Mongo db working

--- client/js/controllers/meetups-controller.js
+++ client/js/controllers/meetups-controller.js
@@ -1,12 +1,18 @@
 app.controller('meetupsController', ['$scope', '$resource', function ($scope, $resource) {
 
+	var Meetup = $resource('/api/meetups');
+
 	$scope.meetups = [
 		{ name: "MEAN SF Developers" },
 		{ name: "Some other meetups" }
 	]
 
 	$scope.createMeetup = function () {
-    	$scope.meetups.push({ name : $scope.meetupName });
-    	$scope.meetupName = '';
+    	var meetup = new Meetup();
+    	meetup.name = $scope.meetupName;
+    	meetup.$save(function (result) {
+    		$scope.meetups.push(result);
+    		$scope.meetupName = '';
+    	});
     }
 }]);
